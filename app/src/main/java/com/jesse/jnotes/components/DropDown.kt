8476fed7a259ed13ffe52cfef242b0e02fd7441f
133413 <133413@hondsrugcollege.nl>
2024-01-15 13:20:52 +0100

    finalize dropdown

--- app/src/main/java/com/jesse/jnotes/components/DropDown.kt
+++ app/src/main/java/com/jesse/jnotes/components/DropDown.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -14,18 +15,28 @@ import androidx.compose.ui.unit.Dp
 @Composable
 fun DropDownMenuComponent(
     content: List<String>,
-    value: MutableState<String>
+    value: (String) -> Unit
 ) {
     Column {
         var expanded by remember { mutableStateOf(false) }
-        Button(onClick = {expanded = !expanded}){
+        Button(onClick = { expanded = !expanded }) {
             Text("test")
         }
-        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }, Modifier.fillMaxWidth().padding(
-            Dp(16F)
-        )) {
+        DropdownMenu(
+            expanded = expanded, onDismissRequest = { expanded = false },
+            Modifier
+                .fillMaxWidth()
+                .padding(
+                    Dp(16F)
+                )
+        ) {
             content.forEach() {
-                Text(it)
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    value( it )
+                }, text = {
+                    Text(it)
+                })
             }
         }
     }
@@ -34,5 +45,11 @@ fun DropDownMenuComponent(
 @Preview
 @Composable
 private fun preview() {
-    DropDownMenuComponent(content = listOf("a", "b", "c"), value = remember { mutableStateOf("hi") })
+    val (state, setState) = remember { mutableStateOf("hi") }
+    Column {
+        Text(state)
+        DropDownMenuComponent(
+            content = listOf("a", "b", "c"),
+            value = setState)
+    }
 }

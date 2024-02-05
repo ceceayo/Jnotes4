1f8a291c19a1466fdad1f4c9a095f68d7ba66f27
ceayo <ceayo@duck.com>
2024-02-05 18:16:33 +0100

    add notecontent.proto and add top bar to ViewFilePage.kt

--- app/src/main/java/com/jesse/jnotes/views/ViewFilePage.kt
+++ app/src/main/java/com/jesse/jnotes/views/ViewFilePage.kt
@@ -2,10 +2,23 @@ package com.jesse.jnotes.views
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.Dp
 import com.jesse.jnotes.logic.StorageApi
 import com.jesse.jnotes.proto.ConfigData
@@ -20,14 +33,14 @@ fun ViewFilePage(
     file: Int, config: MutableState<ConfigData?>, selectedStorageApi: MutableState<StorageApi?>
 ) {
     val note = config.value!!.notesList[file]
-    Scaffold(/*topBar = {
+    Scaffold(topBar = {
         CenterAlignedTopAppBar(
             title = {
-                Text(config.value!!.notesList[file].name)
+                Text(config.value!!.notesList[file].name.capitalize(Locale.current))
             },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)
         )
-    }*/) {
+    }) {
         var text = selectedStorageApi.value!!.getFileContents(arrayOf("notes").plus(note.pathList), "content")
         if (text == null) { text = "" }
         var textEditorState by remember { mutableStateOf(TextEditorState.create(text)) }

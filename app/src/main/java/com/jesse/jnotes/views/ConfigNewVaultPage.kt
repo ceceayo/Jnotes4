f32d0623ab47378e5a551330448208053da016b6
ceayo <ceayo@duck.com>
2024-03-10 12:29:47 +0100

    fuuuuuuuuck new vault screen has a lot of changes

--- app/src/main/java/com/jesse/jnotes/views/ConfigNewVaultPage.kt
+++ app/src/main/java/com/jesse/jnotes/views/ConfigNewVaultPage.kt
@@ -1,6 +1,8 @@
 package com.jesse.jnotes.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,14 +12,19 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import com.jesse.jnotes.components.DropDownMenuComponent
 import com.jesse.jnotes.logic.StorageApi
+import com.jesse.jnotes.logic.blockPlugins
 import com.jesse.jnotes.proto.ConfigData
 import com.jesse.jnotes.proto.configData
 import com.jesse.jnotes.proto.configNoteTypeBlock
@@ -26,6 +33,19 @@ import com.jesse.jnotes.proto.noteType
 import com.jesse.jnotes.views.destinations.FilesPageDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.util.UUID
+
+
+data class ConfigNewVaultPageNoteBlockType(
+    var title: String, var block: String, var config: String
+)
+
+data class ConfigNewVaultPageNoteType(
+    var title: String,
+    var blocks: List<ConfigNewVaultPageNoteBlockType>,
+    val randomUnderTheHoodUUID: UUID
+)
+
 
 @Destination(
 )
@@ -40,6 +60,7 @@ fun ConfigNewVaultPage(
 ) {
     val scrollState = rememberScrollState()
     //val (storageApiValue, setStorageApi) = remember { mutableStateOf("-- please select a storage api --") }
+    var configNoteTypeEditorState = remember { mutableStateListOf<ConfigNewVaultPageNoteType>() }
     Column(
         Modifier
             .fillMaxSize()
@@ -48,7 +69,9 @@ fun ConfigNewVaultPage(
         Icon(
             painter = painterResource(android.R.drawable.ic_menu_preferences),
             contentDescription = "Configuration For JNotes",
-            modifier = Modifier.fillMaxWidth().height(Dp(64F))
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(Dp(64F))
         )
         Text("Pre Launch Configuration for JNotes", Modifier.align(Alignment.CenterHorizontally))
 
@@ -59,49 +82,111 @@ fun ConfigNewVaultPage(
             fileAccessPlugins[storageApiValue]!!.ConfigComponent()
         }
 
-        Button(
-            onClick = {
-                if ((storageApiValue in fileAccessPlugins.keys) and (
-                            fileAccessPlugins[storageApiValue]?.isReady() == true
-                            )
-                ) {
-                    selectedStorageApi.value = fileAccessPlugins[storageApiValue]!!
-                    //config.value = dataConfigurationObject
-                    config.value = configData {
-                        fileStorage = storageApiValue!!
-                        fileStorageOptions = fileAccessPlugins[storageApiValue]!!.saveConfig()
-                        notes += note {
-                            path += "a"
-                            noteType = "test"
-                            name = "my file"
-                        }
-                        notetypes += noteType {
-                            name = "test"
-                            blocks += configNoteTypeBlock {
-                                name = "simple"
-                                blockType = "TextRenderBlock"
-                                this.config = "" // using this. syntax; shadows outer var config
+        Text("Note Types:")
 
-                            }
-                            blocks += configNoteTypeBlock {
-                                name = "simpler"
-                                blockType = "TextRenderBlock"
-                                this.config = "" // using this. syntax; shadows outer var config
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(Dp(16F))
+                .background(Color(0.9F, 0.9F, 0.9F, 1F))
+                .padding(Dp(8F))
+        ) {
+            Row(Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                    configNoteTypeEditorState += listOf(
+                        ConfigNewVaultPageNoteType(
+                            title = "new note type",
+                            blocks = listOf(),
+                            randomUnderTheHoodUUID = UUID.randomUUID()
+                        )
+                    )
+                }) {
+                    Text("Create a new noteType")
+                }
+            }
+            configNoteTypeEditorState.forEachIndexed { idx, noteType ->
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(Dp(8F))
+                        .background(Color(0.8f, 0.8f, 0.8f, 1f))
+                ) {
+                    //Text(noteType.title)
+                    TextField(value = noteType.title, onValueChange = { newTitle ->
+                        configNoteTypeEditorState[idx] = noteType.copy(title = newTitle)
+                    },
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(Dp(4F)))
+                    Row(Modifier.fillMaxWidth()) {
+                        Button(onClick = { /*TODO*/ }, Modifier.padding(Dp(4F))) {
+                            Text("Add Block")
+                        }
+                        Button(onClick = {
+                            configNoteTypeEditorState -= listOf(noteType).toSet()
+                        }, Modifier.padding(Dp(4F))) {
+                            Text("Delete Type")
+                        }
+                    }
+                    Text("Blocks:")
+
+                    noteType.blocks.forEachIndexed { idx, blockType ->
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(Dp(4F))
+                                .background(Color(0.7f, 0.7f, 0.7f, 1f))
+                        ) {
+                            DropDownMenuComponent(content = blockPlugins.keys.toList()) {
+                                println(it)
                             }
                         }
                     }
-                    config.value!!.writeTo(
-                        selectedStorageApi.value!!.setBinFileContents(arrayOf(), "config")
-                            .outputStream()
-                    )
-                    nav.navigate(FilesPageDestination)
                 }
-            },
-            Modifier
-                .fillMaxWidth()
-                .padding(Dp(24F))
-        ) {
-            Text("Go!")
+
+            }
         }
+
+    Button(
+        onClick = {
+            if ((storageApiValue in fileAccessPlugins.keys) and (fileAccessPlugins[storageApiValue]?.isReady() == true)) {
+                selectedStorageApi.value = fileAccessPlugins[storageApiValue]!!
+                //config.value = dataConfigurationObject
+                config.value = configData {
+                    fileStorage = storageApiValue!!
+                    fileStorageOptions = fileAccessPlugins[storageApiValue]!!.saveConfig()
+                    notes += note {
+                        path += "a"
+                        noteType = "test"
+                        name = "my file"
+                    }
+                    notetypes += noteType {
+                        name = "test"
+                        blocks += configNoteTypeBlock {
+                            name = "simple"
+                            blockType = "TextRenderBlock"
+                            this.config = "" // using this. syntax; shadows outer var config
+
+                        }
+                        blocks += configNoteTypeBlock {
+                            name = "simpler"
+                            blockType = "TextRenderBlock"
+                            this.config = "" // using this. syntax; shadows outer var config
+                        }
+                    }
+                }
+                config.value!!.writeTo(
+                    selectedStorageApi.value!!.setBinFileContents(arrayOf(), "config")
+                        .outputStream()
+                )
+                nav.navigate(FilesPageDestination)
+            }
+        },
+        Modifier
+            .fillMaxWidth()
+            .padding(Dp(24F))
+    ) {
+        Text("Go!")
     }
+}
 }

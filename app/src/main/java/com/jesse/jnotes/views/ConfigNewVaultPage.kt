bc87cc1efa49a25fadec1b17266e2f0517afe3fa
ceayo <ceayo@duck.com>
2024-03-10 13:52:24 +0100

    finalize new vault page

--- app/src/main/java/com/jesse/jnotes/views/ConfigNewVaultPage.kt
+++ app/src/main/java/com/jesse/jnotes/views/ConfigNewVaultPage.kt
@@ -3,10 +3,12 @@ package com.jesse.jnotes.views
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -15,12 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.Dp
 import com.jesse.jnotes.components.DropDownMenuComponent
 import com.jesse.jnotes.logic.StorageApi
@@ -28,11 +34,12 @@ import com.jesse.jnotes.logic.blockPlugins
 import com.jesse.jnotes.proto.ConfigData
 import com.jesse.jnotes.proto.configData
 import com.jesse.jnotes.proto.configNoteTypeBlock
-import com.jesse.jnotes.proto.note
 import com.jesse.jnotes.proto.noteType
 import com.jesse.jnotes.views.destinations.FilesPageDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import jp.kaleidot725.texteditor.state.TextEditorState
+import jp.kaleidot725.texteditor.view.TextEditor
 import java.util.UUID
 
 
@@ -42,7 +49,7 @@ data class ConfigNewVaultPageNoteBlockType(
 
 data class ConfigNewVaultPageNoteType(
     var title: String,
-    var blocks: List<ConfigNewVaultPageNoteBlockType>,
+    var blocks: MutableList<ConfigNewVaultPageNoteBlockType>,
     val randomUnderTheHoodUUID: UUID
 )
 
@@ -96,7 +103,7 @@ fun ConfigNewVaultPage(
                     configNoteTypeEditorState += listOf(
                         ConfigNewVaultPageNoteType(
                             title = "new note type",
-                            blocks = listOf(),
+                            blocks = mutableStateListOf(),
                             randomUnderTheHoodUUID = UUID.randomUUID()
                         )
                     )
@@ -119,7 +126,13 @@ fun ConfigNewVaultPage(
                             .fillMaxWidth()
                             .padding(Dp(4F)))
                     Row(Modifier.fillMaxWidth()) {
-                        Button(onClick = { /*TODO*/ }, Modifier.padding(Dp(4F))) {
+                        Button(onClick = {
+                            configNoteTypeEditorState[idx].blocks += ConfigNewVaultPageNoteBlockType(
+                                title = "-- please enter a name for this block --",
+                                "-- please select a block type --",
+                                "-- please type any config for this block here, if needed --"
+                            )
+                        }, Modifier.padding(Dp(4F))) {
                             Text("Add Block")
                         }
                         Button(onClick = {
@@ -137,9 +150,45 @@ fun ConfigNewVaultPage(
                                 .padding(Dp(4F))
                                 .background(Color(0.7f, 0.7f, 0.7f, 1f))
                         ) {
-                            DropDownMenuComponent(content = blockPlugins.keys.toList()) {
-                                println(it)
+                            TextField(value = blockType.title, onValueChange = { newName ->
+                                noteType.blocks[idx] = blockType.copy(title = newName)
+                            },
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(Dp(4F)))
+                            Text("Type: ${blockType.block}")
+                            DropDownMenuComponent(content = blockPlugins.keys.toList()) { blockName ->
+                                noteType.blocks[idx] = blockType.copy(block = blockName)
                             }
+                            Text("Config:")
+                            var textEditorState by remember {
+                                mutableStateOf(
+                                    TextEditorState.create(
+                                        blockType.config
+                                    )
+                                )
+                            }
+                            TextEditor(textEditorState = textEditorState,
+                                onChanged = { newState ->
+                                    textEditorState = newState
+                                    noteType.blocks[idx] =
+                                        blockType.copy(config = textEditorState.getAllText())
+                                },
+                                Modifier
+                                    .fillMaxWidth()
+                                    .height(Dp(300F)),
+                                decorationBox = { index, isSelected, innerTextField ->
+                                    val backgroundColor =
+                                        if (isSelected) Color.Gray else Color.Transparent
+                                    Row(modifier = Modifier.background(backgroundColor)) {
+                                        Text(
+                                            text = (index + 1).toString().padStart(4, '0'),
+                                            fontFamily = FontFamily.Monospace,
+                                        )
+                                        Spacer(modifier = Modifier.width(Dp(4F)))
+                                        innerTextField(Modifier.fillMaxWidth())
+                                    }
+                                })
                         }
                     }
                 }
@@ -147,46 +196,46 @@ fun ConfigNewVaultPage(
             }
         }
 
-    Button(
-        onClick = {
-            if ((storageApiValue in fileAccessPlugins.keys) and (fileAccessPlugins[storageApiValue]?.isReady() == true)) {
-                selectedStorageApi.value = fileAccessPlugins[storageApiValue]!!
-                //config.value = dataConfigurationObject
-                config.value = configData {
-                    fileStorage = storageApiValue!!
-                    fileStorageOptions = fileAccessPlugins[storageApiValue]!!.saveConfig()
-                    notes += note {
-                        path += "a"
-                        noteType = "test"
-                        name = "my file"
-                    }
-                    notetypes += noteType {
-                        name = "test"
-                        blocks += configNoteTypeBlock {
-                            name = "simple"
-                            blockType = "TextRenderBlock"
-                            this.config = "" // using this. syntax; shadows outer var config
-
-                        }
-                        blocks += configNoteTypeBlock {
-                            name = "simpler"
-                            blockType = "TextRenderBlock"
-                            this.config = "" // using this. syntax; shadows outer var config
+        Button(
+            onClick = {
+                configNoteTypeEditorState.forEach { noteType ->
+                    noteType.blocks.forEach { blockType ->
+                        if(!blockPlugins.containsKey(blockType.block)) {
+                            return@Button
                         }
                     }
                 }
-                config.value!!.writeTo(
-                    selectedStorageApi.value!!.setBinFileContents(arrayOf(), "config")
-                        .outputStream()
-                )
-                nav.navigate(FilesPageDestination)
-            }
-        },
-        Modifier
-            .fillMaxWidth()
-            .padding(Dp(24F))
-    ) {
-        Text("Go!")
+                if ((storageApiValue in fileAccessPlugins.keys) and (fileAccessPlugins[storageApiValue]?.isReady() == true)) {
+                    selectedStorageApi.value = fileAccessPlugins[storageApiValue]!!
+                    //config.value = dataConfigurationObject
+                    config.value = configData {
+                        fileStorage = storageApiValue!!
+                        fileStorageOptions = fileAccessPlugins[storageApiValue]!!.saveConfig()
+                        configNoteTypeEditorState.forEach { NoteTypeIt ->
+                            notetypes += noteType {
+                                name = NoteTypeIt.title
+                                NoteTypeIt.blocks.forEach { NoteBlockIt ->
+                                    blocks += configNoteTypeBlock {
+                                        name = NoteBlockIt.title
+                                        blockType = NoteBlockIt.block
+                                        this.config = NoteBlockIt.config
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    config.value!!.writeTo(
+                        selectedStorageApi.value!!.setBinFileContents(arrayOf(), "config")
+                            .outputStream()
+                    )
+                    nav.navigate(FilesPageDestination)
+                }
+            },
+            Modifier
+                .fillMaxWidth()
+                .padding(Dp(24F))
+        ) {
+            Text("Go!")
+        }
     }
-}
 }

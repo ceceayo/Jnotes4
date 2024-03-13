04adb628263c32e32d6458e6c891d1f327be95b1
133413 <133413@hondsrugcollege.nl>
2024-03-13 15:29:44 +0100

    Nicer welcome page

--- app/src/main/java/com/jesse/jnotes/views/ConfigPage.kt
+++ app/src/main/java/com/jesse/jnotes/views/ConfigPage.kt
@@ -4,9 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -22,36 +20,77 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @RootNavGraph(start = true)
 @Destination
 @Composable
 fun ConfigPage(
-    navigator: DestinationsNavigator,
-    config: MutableState<ConfigData?>
+    navigator: DestinationsNavigator, config: MutableState<ConfigData?>
 ) {
     config.value = null
-    Scaffold() {
-        Column() {
-            Text("JNotes")
-            Button(onClick = {
-                navigator.navigate(ConfigNewVaultPageDestination(ConfigNewVaultPageDestination.NavArgs(null)))
-
-            }, Modifier
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("JNotes") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onSecondaryContainer
+                )
+            )
+        }
+    ) {
+        Column(
+            Modifier
                 .fillMaxWidth()
-                .padding(Dp(16F))
+                .padding(it)
+        ) {
+            Button(
+                onClick = {
+                    navigator.navigate(
+                        ConfigNewVaultPageDestination(
+                            ConfigNewVaultPageDestination.NavArgs(
+                                null
+                            )
+                        )
+                    )
+
+                },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(Dp(16F))
             ) {
                 Text("Create a new vault")
             }
-            Text("or", Modifier.fillMaxWidth().drawBehind {
-                this.drawLine(Color.Black, Offset(20f, this.center.y), Offset(this.center.x - 50f, this.center.y))
-                this.drawLine(Color.Black, Offset(this.center.x + 50f, this.center.y), Offset(this.size.width - 20f, this.center.y))
-            }, textAlign = TextAlign.Center)
-            Button(onClick = {
-                navigator.navigate(ConfigLoadVaultPageDestination(ConfigLoadVaultPageDestination.NavArgs(null)))
-            }, Modifier
-                .fillMaxWidth()
-                .padding(Dp(16F))
+            Text("or",
+                Modifier
+                    .fillMaxWidth()
+                    .drawBehind {
+                        this.drawLine(
+                            Color.Black,
+                            Offset(20f, this.center.y),
+                            Offset(this.center.x - 50f, this.center.y)
+                        )
+                        this.drawLine(
+                            Color.Black,
+                            Offset(this.center.x + 50f, this.center.y),
+                            Offset(this.size.width - 20f, this.center.y)
+                        )
+                    }, textAlign = TextAlign.Center
+            )
+            Button(
+                onClick = {
+                    navigator.navigate(
+                        ConfigLoadVaultPageDestination(
+                            ConfigLoadVaultPageDestination.NavArgs(
+                                null
+                            )
+                        )
+                    )
+                },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(Dp(16F))
             ) {
                 Text("Load a vault")
             }
